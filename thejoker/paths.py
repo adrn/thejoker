d11be00a8601c3f47f5fa95f42aef717d31e87ab
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-09-04 16:51:39 -0400

    init and a module for handling paths

---
+++ thejoker/paths.py
@@ -0,0 +1,30 @@
+# Standard library
+import os
+from os.path import abspath, join, split, exists
+
+__all__ = ['Paths']
+
+class Paths(object):
+
+    """
+    A class that maintains various project paths for making plots and
+    caching intermediate data products.
+
+    Parameters
+    ----------
+    script__file__ : str
+        Called from within a script in the ``scripts`` path, this should be
+        the builtin ``__file__`` variable.
+
+    """
+
+    def __init__(self, script__file__):
+        self.root = abspath(join(split(abspath(script__file__))[0], ".."))
+
+        self.cache = join(self.root, "cache")
+        self.plots = join(self.root, "plots")
+        self.figures = join(self.root, "paper", "figures")
+
+        for path in [self.cache, self.plots, self.figures]:
+            if not path.exists:
+                os.makedirs(str(path))
