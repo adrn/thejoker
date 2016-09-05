3e4bfb3272dd4979c587f653eecba36794ef4588
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-09-04 22:37:30 -0400

    enable loadbalancing and add slurm submit script

---
+++ slurm/submit-stampede.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -J sampler           # job name
+#SBATCH -o sampler.o%j             # output file name (%j expands to jobID)
+#SBATCH -e sampler.e%j             # error file name (%j expands to jobID)
+#SBATCH -n 256                   # total number of mpi tasks requested
+#SBATCH -p normal          # queue (partition) -- normal, development, etc.
+#SBATCH -A TG-AST150023         # Project ID
+#SBATCH -t 02:00:00             # run time (hh:mm:ss) - 1.5 hours
+#SBATCH --mail-user=adrn@princeton.edu
+#SBATCH --mail-type=begin       # email me when the job starts
+#SBATCH --mail-type=end         # email me when the job finishes
+
+cd $WORK/projects/ebak/scripts/
+
+source activate thejoker
+
+ibrun python run-sampler.py -v --id='2M03080601+7950502' --mpi -n 2**24
+ibrun python run-sampler.py -v --id='2M00110648+6609349' --mpi -n 2**24
+ibrun python run-sampler.py -v --id='2M00344509+8512058' --mpi -n 2**24
