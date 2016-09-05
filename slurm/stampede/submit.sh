71e6b39f0d5f0bd384b28ba4ec6fbe2ba4d1b967
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-09-05 16:24:14 -0500

    back to before

--- slurm/stampede/submit.sh
+++ slurm/stampede/submit.sh
@@ -2,7 +2,7 @@
 #SBATCH -J sampler           # job name
 #SBATCH -o sampler.o%j             # output file name (%j expands to jobID)
 #SBATCH -e sampler.e%j             # error file name (%j expands to jobID)
-#SBATCH -n 257                   # total number of mpi tasks requested
+#SBATCH -n 256                   # total number of mpi tasks requested
 #SBATCH -p normal          # queue (partition) -- normal, development, etc.
 #SBATCH -A TG-AST150023         # Project ID
 #SBATCH -t 02:00:00             # run time (hh:mm:ss) - 1.5 hours
@@ -14,6 +14,6 @@ cd $WORK/projects/thejoker/scripts/
 
 source activate thejoker
 
-ibrun python run-sampler.py -v --id='2M03080601+7950502' --mpi -n 2**20
-# ibrun python run-sampler.py -v --id='2M00110648+6609349' --mpi -n 2**20
-# ibrun python run-sampler.py -v --id='2M00344509+8512058' --mpi -n 2**20
+ibrun python run-sampler.py -v --id='2M03080601+7950502' --mpi -n 2**24
+# ibrun python run-sampler.py -v --id='2M00110648+6609349' --mpi -n 2**24
+# ibrun python run-sampler.py -v --id='2M00344509+8512058' --mpi -n 2**24
