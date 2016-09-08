872adae5e884be65d7b21ab379a3c747170e7693
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-09-08 11:56:39 -0400

    added slurm script to do experiment1

---
+++ slurm/perseus/experiment1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -J exp1           # job name
+#SBATCH -o exp1.o%j             # output file name (%j expands to jobID)
+#SBATCH -e exp1.e%j             # error file name (%j expands to jobID)
+#SBATCH -n 256                   # total number of mpi tasks requested
+#SBATCH -t 00:30:00             # run time (hh:mm:ss) - 1.5 hours
+#SBATCH --mail-user=adrn@princeton.edu
+#SBATCH --mail-type=begin       # email me when the job starts
+#SBATCH --mail-type=end         # email me when the job finishes
+
+cd /tigress/adrianp/projects/thejoker/scripts/
+
+module load openmpi/gcc/1.10.2/64
+
+source activate thejoker
+
+# Run experiment 1!
+srun python run-sampler.py -v --mpi -o \
+-n 2**27 \
+-f ../data/troup-allVisit.h5 \
+--hdf5-key="2M00110648+6609349"
