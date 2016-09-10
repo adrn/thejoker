3eac4dd077c0b9679f439e2cc50042733be8b541
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-09-09 21:26:55 -0400

    MOAR SAMPLEZ
    Former-commit-id: 1ce858813e3ca62013d9ca597f4e5f269f8e0c97

--- slurm/perseus/experiment2.sh
+++ slurm/perseus/experiment2.sh
@@ -16,60 +16,60 @@ source activate thejoker
 
 python make-experiment2-data.py -s 42
 
-export NSAMPLES="2**27"
+export NSAMPLES="2**28"
 export SEED=42
 
 # Run experiment 2, dropping data points in batches of 2
-srun python run-sampler.py -q --mpi -o \
+srun python run-sampler.py -v --mpi -o \
 -n $NSAMPLES \
 -f ../data/experiment2.h5 \
 --name="experiment2-0.hdf5" \
 --hdf5-key="0" \
 --seed=$SEED
 
-srun python run-sampler.py -q --mpi -o \
+srun python run-sampler.py -v --mpi -o \
 -n $NSAMPLES \
 -f ../data/experiment2.h5 \
 --name="experiment2-2.hdf5" \
 --hdf5-key="2" \
 --seed=$SEED
 
-srun python run-sampler.py -q --mpi -o \
+srun python run-sampler.py -v --mpi -o \
 -n $NSAMPLES \
 -f ../data/experiment2.h5 \
 --name="experiment2-4.hdf5" \
 --hdf5-key="4" \
 --seed=$SEED
 
-srun python run-sampler.py -q --mpi -o \
+srun python run-sampler.py -v --mpi -o \
 -n $NSAMPLES \
 -f ../data/experiment2.h5 \
 --name="experiment2-6.hdf5" \
 --hdf5-key="6" \
 --seed=$SEED
 
-srun python run-sampler.py -q --mpi -o \
+srun python run-sampler.py -v --mpi -o \
 -n $NSAMPLES \
 -f ../data/experiment2.h5 \
 --name="experiment2-8.hdf5" \
 --hdf5-key="8" \
 --seed=$SEED
 
-srun python run-sampler.py -q --mpi -o \
+srun python run-sampler.py -v --mpi -o \
 -n $NSAMPLES \
 -f ../data/experiment2.h5 \
 --name="experiment2-10.hdf5" \
 --hdf5-key="10" \
 --seed=$SEED
 
-srun python run-sampler.py -q --mpi -o \
+srun python run-sampler.py -v --mpi -o \
 -n $NSAMPLES \
 -f ../data/experiment2.h5 \
 --name="experiment2-12.hdf5" \
 --hdf5-key="12" \
 --seed=$SEED
 
-srun python run-sampler.py -q --mpi -o \
+srun python run-sampler.py -v --mpi -o \
 -n $NSAMPLES \
 -f ../data/experiment2.h5 \
 --name="experiment2-14.hdf5" \
