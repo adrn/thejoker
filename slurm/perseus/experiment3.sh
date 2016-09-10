b0556c3a55c9107ee284b44c8a8f8fbb6027351b
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-09-10 15:59:49 -0400

    change names of later experiments
    Former-commit-id: 89ce3e59e96b01b0304c40f5ee5e1a2a1e052efd

--- slurm/perseus/experiment3.sh
+++ slurm/perseus/experiment3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -J exp2           # job name
-#SBATCH -o exp2.o%j             # output file name (%j expands to jobID)
-#SBATCH -e exp2.e%j             # error file name (%j expands to jobID)
+#SBATCH -J exp3           # job name
+#SBATCH -o exp3.o%j             # output file name (%j expands to jobID)
+#SBATCH -e exp3.e%j             # error file name (%j expands to jobID)
 #SBATCH -n 512                   # total number of mpi tasks requested
 #SBATCH -t 01:45:00             # run time (hh:mm:ss)
 #SBATCH --mail-user=adrn@princeton.edu
@@ -14,64 +14,64 @@ module load openmpi/gcc/1.10.2/64
 
 source activate thejoker
 
-python make-experiment2-data.py -s 42
+python make-experiment3-data.py -s 42
 
 export NSAMPLES="2**28"
 export SEED=42
 
-# Run experiment 2, dropping data points in batches of 2
+# Run experiment 3, dropping data points in batches of 2
 srun python run-sampler.py -v --mpi -o \
 -n $NSAMPLES \
--f ../data/experiment2.h5 \
---name="experiment2-0.hdf5" \
+-f ../data/experiment3.h5 \
+--name="experiment3-0.hdf5" \
 --hdf5-key="0" \
 --seed=$SEED
 
 srun python run-sampler.py -v --mpi -o \
 -n $NSAMPLES \
--f ../data/experiment2.h5 \
---name="experiment2-2.hdf5" \
+-f ../data/experiment3.h5 \
+--name="experiment3-2.hdf5" \
 --hdf5-key="2" \
 --seed=$SEED
 
 srun python run-sampler.py -v --mpi -o \
 -n $NSAMPLES \
--f ../data/experiment2.h5 \
---name="experiment2-4.hdf5" \
+-f ../data/experiment3.h5 \
+--name="experiment3-4.hdf5" \
 --hdf5-key="4" \
 --seed=$SEED
 
 srun python run-sampler.py -v --mpi -o \
 -n $NSAMPLES \
--f ../data/experiment2.h5 \
---name="experiment2-6.hdf5" \
+-f ../data/experiment3.h5 \
+--name="experiment3-6.hdf5" \
 --hdf5-key="6" \
 --seed=$SEED
 
 srun python run-sampler.py -v --mpi -o \
 -n $NSAMPLES \
--f ../data/experiment2.h5 \
---name="experiment2-8.hdf5" \
+-f ../data/experiment3.h5 \
+--name="experiment3-8.hdf5" \
 --hdf5-key="8" \
 --seed=$SEED
 
 srun python run-sampler.py -v --mpi -o \
 -n $NSAMPLES \
--f ../data/experiment2.h5 \
---name="experiment2-10.hdf5" \
+-f ../data/experiment3.h5 \
+--name="experiment3-10.hdf5" \
 --hdf5-key="10" \
 --seed=$SEED
 
 srun python run-sampler.py -v --mpi -o \
 -n $NSAMPLES \
--f ../data/experiment2.h5 \
---name="experiment2-12.hdf5" \
+-f ../data/experiment3.h5 \
+--name="experiment3-12.hdf5" \
 --hdf5-key="12" \
 --seed=$SEED
 
 srun python run-sampler.py -v --mpi -o \
 -n $NSAMPLES \
--f ../data/experiment2.h5 \
---name="experiment2-14.hdf5" \
+-f ../data/experiment3.h5 \
+--name="experiment3-14.hdf5" \
 --hdf5-key="14" \
 --seed=$SEED
