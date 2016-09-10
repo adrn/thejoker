040a0343e71853ced02a2b09279d4107edf89070
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-09-10 17:00:33 -0400

    also produce data for experiment 5
    Former-commit-id: c6871ca61c5ef33180269570a21ab69431326d55

--- slurm/perseus/experiment5.sh
+++ slurm/perseus/experiment5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -J exp1           # job name
-#SBATCH -o exp1.o%j             # output file name (%j expands to jobID)
-#SBATCH -e exp1.e%j             # error file name (%j expands to jobID)
+#SBATCH -J exp5           # job name
+#SBATCH -o exp5.o%j             # output file name (%j expands to jobID)
+#SBATCH -e exp5.e%j             # error file name (%j expands to jobID)
 #SBATCH -n 256                   # total number of mpi tasks requested
 #SBATCH -t 00:30:00             # run time (hh:mm:ss) - 1.5 hours
 #SBATCH --mail-user=adrn@princeton.edu
@@ -14,10 +14,10 @@ module load openmpi/gcc/1.10.2/64
 
 source activate thejoker
 
-# Run experiment 1!
+# Run experiment 5!
 python make-experiment15-data.py -s 1988
 
 srun python run-sampler.py -v --mpi -o \
 -n 2**28 -s 42 \
--f ../data/experiment1.h5 \
---name="experiment1.h5"
+-f ../data/experiment5.h5 \
+--name="experiment5.h5"
