d85f31307ef5d74b1a5bc33d3a02951e9f295128
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-09-10 14:18:40 -0400

    change seed
    Former-commit-id: 4722af7ddaf944242fe56936dc9feb5839ad20a7

--- slurm/perseus/experiment1.sh
+++ slurm/perseus/experiment1.sh
@@ -15,7 +15,7 @@ module load openmpi/gcc/1.10.2/64
 source activate thejoker
 
 # Run experiment 1!
-python make-experiment1-data.py -s 42
+python make-experiment1-data.py -s 1988
 
 srun python run-sampler.py -v --mpi -o \
 -n 2**28 \
