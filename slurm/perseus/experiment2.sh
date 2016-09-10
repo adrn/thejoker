89ce3e59e96b01b0304c40f5ee5e1a2a1e052efd
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-09-10 15:59:49 -0400

    change names of later experiments

--- slurm/perseus/experiment2.sh
+++ slurm/perseus/experiment2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH -J exp1           # job name
-#SBATCH -o exp1.o%j             # output file name (%j expands to jobID)
-#SBATCH -e exp1.e%j             # error file name (%j expands to jobID)
-#SBATCH -n 256                   # total number of mpi tasks requested
-#SBATCH -t 00:30:00             # run time (hh:mm:ss) - 1.5 hours
+#SBATCH -J exp2           # job name
+#SBATCH -o exp2.o%j             # output file name (%j expands to jobID)
+#SBATCH -e exp2.e%j             # error file name (%j expands to jobID)
+#SBATCH -n 512                   # total number of mpi tasks requested
+#SBATCH -t 00:12:00             # run time (hh:mm:ss) - 1.5 hours
 #SBATCH --mail-user=adrn@princeton.edu
 #SBATCH --mail-type=begin       # email me when the job starts
 #SBATCH --mail-type=end         # email me when the job finishes
@@ -14,9 +14,9 @@ module load openmpi/gcc/1.10.2/64
 
 source activate thejoker
 
-# Run experiment 1!
+# Run experiment 2!
 srun python run-sampler.py -v --mpi -o \
 -n 2**28 \
 -f ../data/troup-allVisit.h5 \
 --hdf5-key="2M00110648+6609349" \
---name="experiment1.h5"
+--name="experiment2.h5"
