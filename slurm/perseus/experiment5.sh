aeba4fb7d452bf05d637d35eaa2fb7522b615eb3
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-09-11 13:38:08 -0400

    change length of job
    Former-commit-id: 9f8de9be37c41387fa4627346eba87acaf7aefc7

--- slurm/perseus/experiment5.sh
+++ slurm/perseus/experiment5.sh
@@ -2,8 +2,8 @@
 #SBATCH -J exp5           # job name
 #SBATCH -o exp5.o%j             # output file name (%j expands to jobID)
 #SBATCH -e exp5.e%j             # error file name (%j expands to jobID)
-#SBATCH -n 256                   # total number of mpi tasks requested
-#SBATCH -t 00:30:00             # run time (hh:mm:ss) - 1.5 hours
+#SBATCH -n 512                   # total number of mpi tasks requested
+#SBATCH -t 02:00:00             # run time (hh:mm:ss) - 1.5 hours
 #SBATCH --mail-user=adrn@princeton.edu
 #SBATCH --mail-type=begin       # email me when the job starts
 #SBATCH --mail-type=end         # email me when the job finishes
