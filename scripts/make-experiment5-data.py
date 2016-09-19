1369f379f9a4b8c3d52a438d7be7bff494a1c5eb
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-09-19 13:06:57 -0400

    doh, fix phasey stuff

--- scripts/make-experiment5-data.py
+++ scripts/make-experiment5-data.py
@@ -38,9 +38,9 @@ def main():
     _t = (np.array([0.02, 4.08, 4.45, 4.47]) + f0) * P
 
     ts = [
-        np.concatenate((_t, np.array([6.46, 7.04 + f0]) * P)) + EPOCH,
-        np.concatenate((_t, np.array([6.04, 6.07 + f0]) * P)) + EPOCH,
-        np.concatenate((_t, np.array([6.62, 6.65 + f0]) * P)) + EPOCH
+        np.concatenate((_t, (np.array([6.46, 7.04]) + f0) * P)) + EPOCH,
+        np.concatenate((_t, (np.array([6.04, 6.07]) + f0) * P)) + EPOCH,
+        np.concatenate((_t, (np.array([6.62, 6.65]) + f0) * P)) + EPOCH
     ]
 
     rv_err = np.random.uniform(0.2, 0.3, size=ts[0].size) * u.km/u.s
