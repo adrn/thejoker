a4f31659074c07d4229913679cc44bbec9d3920c
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-10-08 17:35:34 -0400

    fix script

--- slurm/perseus/experiment2.sh
+++ slurm/perseus/experiment2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH -J exp4           # job name
-#SBATCH -o exp4.o%j             # output file name (%j expands to jobID)
-#SBATCH -e exp4.e%j             # error file name (%j expands to jobID)
+#SBATCH -J exp2           # job name
+#SBATCH -o exp2.o%j             # output file name (%j expands to jobID)
+#SBATCH -e exp2.e%j             # error file name (%j expands to jobID)
 #SBATCH -n 512                   # total number of mpi tasks requested
 #SBATCH -t 01:45:00             # run time (hh:mm:ss)
 #SBATCH --mail-user=adrn@princeton.edu
@@ -19,31 +19,31 @@ python make-experiment2-data.py -s 42
 export NSAMPLES="2**28"
 export SEED=42
 
-# Run experiment 4, inflating error-bars by factor of 2, 4, 8, 16
+# Run experiment 2, inflating error-bars by factor of 1, 2, 4, 8
 srun python run-sampler.py -v --mpi -o \
 -n $NSAMPLES \
--f ../data/experiment4.h5 \
---name="experiment4-2.hdf5" \
+-f ../data/experiment2.h5 \
+--name="experiment2-1.hdf5" \
+--hdf5-key="1" \
+--seed=$SEED
+
+srun python run-sampler.py -v --mpi -o \
+-n $NSAMPLES \
+-f ../data/experiment2.h5 \
+--name="experiment2-2.hdf5" \
 --hdf5-key="2" \
 --seed=$SEED
 
 srun python run-sampler.py -v --mpi -o \
 -n $NSAMPLES \
--f ../data/experiment4.h5 \
---name="experiment4-4.hdf5" \
+-f ../data/experiment2.h5 \
+--name="experiment2-4.hdf5" \
 --hdf5-key="4" \
 --seed=$SEED
 
 srun python run-sampler.py -v --mpi -o \
 -n $NSAMPLES \
--f ../data/experiment4.h5 \
---name="experiment4-8.hdf5" \
+-f ../data/experiment2.h5 \
+--name="experiment2-8.hdf5" \
 --hdf5-key="8" \
---seed=$SEED
-
-srun python run-sampler.py -v --mpi -o \
--n $NSAMPLES \
--f ../data/experiment4.h5 \
---name="experiment4-16.hdf5" \
---hdf5-key="16" \
 --seed=$SEED
