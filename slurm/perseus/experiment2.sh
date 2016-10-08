2ea3d11766b9e507214a8d31d1eb18b473e2d540
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-10-08 17:33:26 -0400

    modify experiment 1 and 2
    Former-commit-id: 9dd23aeebb9449aebcbb0d9febeb4873cc1e3e05

--- slurm/perseus/experiment2.sh
+++ slurm/perseus/experiment2.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH -J exp2           # job name
-#SBATCH -o exp2.o%j             # output file name (%j expands to jobID)
-#SBATCH -e exp2.e%j             # error file name (%j expands to jobID)
+#SBATCH -J exp4           # job name
+#SBATCH -o exp4.o%j             # output file name (%j expands to jobID)
+#SBATCH -e exp4.e%j             # error file name (%j expands to jobID)
 #SBATCH -n 512                   # total number of mpi tasks requested
-#SBATCH -t 00:12:00             # run time (hh:mm:ss) - 1.5 hours
+#SBATCH -t 01:45:00             # run time (hh:mm:ss)
 #SBATCH --mail-user=adrn@princeton.edu
 #SBATCH --mail-type=begin       # email me when the job starts
 #SBATCH --mail-type=end         # email me when the job finishes
@@ -14,9 +14,36 @@ module load openmpi/gcc/1.10.2/64
 
 source activate thejoker
 
-# Run experiment 2!
+python make-experiment2-data.py -s 42
+
+export NSAMPLES="2**28"
+export SEED=42
+
+# Run experiment 4, inflating error-bars by factor of 2, 4, 8, 16
 srun python run-sampler.py -v --mpi -o \
--n 2**28 -s 42 \
--f ../data/troup-allVisit.h5 \
---hdf5-key="2M00110648+6609349" \
---name="experiment2.h5"
+-n $NSAMPLES \
+-f ../data/experiment4.h5 \
+--name="experiment4-2.hdf5" \
+--hdf5-key="2" \
+--seed=$SEED
+
+srun python run-sampler.py -v --mpi -o \
+-n $NSAMPLES \
+-f ../data/experiment4.h5 \
+--name="experiment4-4.hdf5" \
+--hdf5-key="4" \
+--seed=$SEED
+
+srun python run-sampler.py -v --mpi -o \
+-n $NSAMPLES \
+-f ../data/experiment4.h5 \
+--name="experiment4-8.hdf5" \
+--hdf5-key="8" \
+--seed=$SEED
+
+srun python run-sampler.py -v --mpi -o \
+-n $NSAMPLES \
+-f ../data/experiment4.h5 \
+--name="experiment4-16.hdf5" \
+--hdf5-key="16" \
+--seed=$SEED
