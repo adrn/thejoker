ebf5f055590c95a51ee8c04bc32e2bf18374a4f5
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-10-09 16:27:11 -0400

    fix

--- slurm/perseus/experiment4.sh
+++ slurm/perseus/experiment4.sh
@@ -2,8 +2,8 @@
 #SBATCH -J exp4           # job name
 #SBATCH -o exp4.o%j             # output file name (%j expands to jobID)
 #SBATCH -e exp4.e%j             # error file name (%j expands to jobID)
-#SBATCH -n 512                   # total number of mpi tasks requested
-#SBATCH -t 00:12:00             # run time (hh:mm:ss) - 1.5 hours
+#SBATCH -n 260                   # total number of mpi tasks requested
+#SBATCH -t 00:26:00             # run time (hh:mm:ss) - 1.5 hours
 #SBATCH --mail-user=adrn@princeton.edu
 #SBATCH --mail-type=begin       # email me when the job starts
 #SBATCH --mail-type=end         # email me when the job finishes
@@ -26,5 +26,5 @@ srun python run-sampler.py -v --mpi -o \
 -n 2**28 -s 42 \
 -f ../data/troup-allVisit.h5 \
 --hdf5-key="2M00110648+6609349" \
---name="experiment4-fixed-jitter.h5" \
+--name="experiment4-sample-jitter.h5" \
 --log-jitter2-mean=10.5 --log-jitter2-std=1.
