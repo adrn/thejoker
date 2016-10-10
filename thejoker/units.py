5bdb4bd6d2340b628a07e8b71fbdba1ed705bbac
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-10-10 11:38:18 -0400

    oops these too
    Former-commit-id: 6fa324e763aba2b6cd1bba8a9d5bfa601e7990d5

--- thejoker/units.py
+++ thejoker/units.py
@@ -9,7 +9,7 @@ import astropy.units as u
 
 __all__ = ['default_units']
 
-default_units = OrderedDict()
+default_units = dict()
 default_units['P'] = u.day
 default_units['K'] = u.m/u.s
 default_units['ecc'] = u.one
@@ -20,11 +20,11 @@ default_units['jitter'] = u.m/u.s
 default_units['asini'] = u.Rsun
 default_units['mf'] = u.Msun
 
-default_plot_units = OrderedDict()
-default_plot_units['P'] = u.day
-default_plot_units['K'] = u.m/u.s
-default_plot_units['ecc'] = u.one
-default_plot_units['omega'] = u.degree
-default_plot_units['phi0'] = u.degree
-default_plot_units['v0'] = u.km/u.s
-default_plot_units['jitter'] = u.m/u.s
+# default_plot_units = dict()
+# default_plot_units['P'] = u.day
+# default_plot_units['K'] = u.m/u.s
+# default_plot_units['ecc'] = u.one
+# default_plot_units['omega'] = u.degree
+# default_plot_units['phi0'] = u.degree
+# default_plot_units['v0'] = u.km/u.s
+# default_plot_units['jitter'] = u.m/u.s
