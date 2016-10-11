7077c4187f393cb434527d1676e3e708ba0234cb
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-10-10 21:09:05 -0400

    change std
    Former-commit-id: b30dec5f9ae6d1527915b94e5487f5fb024f9b38

--- slurm/perseus/experiment1.sh
+++ slurm/perseus/experiment1.sh
@@ -15,16 +15,16 @@ module load openmpi/gcc/1.10.2/64
 source activate thejoker
 
 # Run experiment 1!
-python make-experiment1-data.py -s 1234
+# python make-experiment1-data.py -s 1234
 
-srun python run-sampler.py -v --mpi -o \
--n 2**28 -s 42 \
--f ../data/experiment1.h5 \
---name="experiment1-fixed-jitter.h5" \
---fixed-jitter='0 m/s'
+# srun python run-sampler.py -v --mpi -o \
+# -n 2**28 -s 42 \
+# -f ../data/experiment1.h5 \
+# --name="experiment1-fixed-jitter.h5" \
+# --fixed-jitter='0 m/s'
 
 srun python run-sampler.py -v --mpi -o \
 -n 2**28 -s 42 \
 -f ../data/experiment1.h5 \
 --name="experiment1-sample-jitter.h5" \
---log-jitter2-mean=0 --log-jitter2-std=4
+--log-jitter2-mean=0 --log-jitter2-std=8
