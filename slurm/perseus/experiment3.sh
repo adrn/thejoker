6227d318f2a03cce745ad1b9d6f54ec196b370be
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-10-11 20:32:37 -0400

    update experiment3

--- slurm/perseus/experiment3.sh
+++ slurm/perseus/experiment3.sh
@@ -2,8 +2,8 @@
 #SBATCH -J exp3           # job name
 #SBATCH -o exp3.o%j             # output file name (%j expands to jobID)
 #SBATCH -e exp3.e%j             # error file name (%j expands to jobID)
-#SBATCH -n 512                   # total number of mpi tasks requested
-#SBATCH -t 01:45:00             # run time (hh:mm:ss)
+#SBATCH -n 256                   # total number of mpi tasks requested
+#SBATCH -t 00:50:00             # run time (hh:mm:ss)
 #SBATCH --mail-user=adrn@princeton.edu
 #SBATCH --mail-type=begin       # email me when the job starts
 #SBATCH --mail-type=end         # email me when the job finishes
@@ -19,36 +19,11 @@ python make-experiment3-data.py -s 42
 export NSAMPLES="2**28"
 export SEED=42
 
-# Run experiment 3, dropping data points in batches of 2
 srun python run-sampler.py -v --mpi -o \
 -n $NSAMPLES \
 -f ../data/experiment3.h5 \
---name="experiment3-0.hdf5" \
---hdf5-key="0" \
---seed=$SEED \
---fixed-jitter='0 m/s'
-
-srun python run-sampler.py -v --mpi -o \
--n $NSAMPLES \
--f ../data/experiment3.h5 \
---name="experiment3-2.hdf5" \
---hdf5-key="2" \
---seed=$SEED \
---fixed-jitter='0 m/s'
-
-srun python run-sampler.py -v --mpi -o \
--n $NSAMPLES \
--f ../data/experiment3.h5 \
---name="experiment3-4.hdf5" \
---hdf5-key="4" \
---seed=$SEED \
---fixed-jitter='0 m/s'
-
-srun python run-sampler.py -v --mpi -o \
--n $NSAMPLES \
--f ../data/experiment3.h5 \
---name="experiment3-6.hdf5" \
---hdf5-key="6" \
+--name="experiment3-16.hdf5" \
+--hdf5-key="16" \
 --seed=$SEED \
 --fixed-jitter='0 m/s'
 
@@ -63,23 +38,15 @@ srun python run-sampler.py -v --mpi -o \
 srun python run-sampler.py -v --mpi -o \
 -n $NSAMPLES \
 -f ../data/experiment3.h5 \
---name="experiment3-10.hdf5" \
---hdf5-key="10" \
+--name="experiment3-4.hdf5" \
+--hdf5-key="4" \
 --seed=$SEED \
 --fixed-jitter='0 m/s'
 
 srun python run-sampler.py -v --mpi -o \
 -n $NSAMPLES \
 -f ../data/experiment3.h5 \
---name="experiment3-12.hdf5" \
---hdf5-key="12" \
---seed=$SEED \
---fixed-jitter='0 m/s'
-
-srun python run-sampler.py -v --mpi -o \
--n $NSAMPLES \
--f ../data/experiment3.h5 \
---name="experiment3-14.hdf5" \
---hdf5-key="14" \
+--name="experiment3-2.hdf5" \
+--hdf5-key="2" \
 --seed=$SEED \
 --fixed-jitter='0 m/s'
