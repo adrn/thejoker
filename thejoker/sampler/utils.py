dee7e6afda3bd9190fcee01276669d33fb3f52be
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-01-04 16:47:41 -0500

    docstring

--- thejoker/sampler/utils.py
+++ thejoker/sampler/utils.py
@@ -1,4 +1,16 @@
 from __future__ import division, print_function
 
 def get_ivar(data, s2):
+    """
+    Return a copy of the inverse variance array with jitter included.
+
+    This is safe for zero'd out inverse variances.
+
+    Parameters
+    ----------
+    data : `~thejoker.data.RVData`
+    s2 : numeric
+        Jitter squared in the same units as the RV data.
+
+    """
     return data.ivar.value / (1 + s2 * data.ivar.value)
