bffc6026fcc1fde9cb095542b405a1b9bb15c88e
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-01-05 15:31:09 -0500

    cleaning up and docs

--- thejoker/sampler/multiproc_helpers.py
+++ thejoker/sampler/multiproc_helpers.py
@@ -1,31 +1,32 @@
 # Third-party
-from astropy import log as logger
 import h5py
 import numpy as np
 
 # Project
+from ..log import log
 from .utils import get_ivar
 from .likelihood import design_matrix, tensor_vector_scalar, marginal_ln_likelihood
 
-__all__ = ['get_good_samples', 'samples_to_orbital_params']
+__all__ = ['get_good_sample_indices', 'samples_to_orbital_params']
 
 def _marginal_ll_worker(task):
     """
-    Compute the marginal log-likelihood integrated over the linear parameters
-    v0 and asini (or velocity semi-amplitude, K). This is meant to be
-    ``map``ped using one of the ``Pool`` classes by the functions below and
-    is not supposed to be in the public API.
+    Compute the marginal log-likelihood, i.e. the likelihood integrated over
+    the linear parameters. This is meant to be ``map``ped using a processing
+    pool` within the functions below and is not supposed to be in the
+    public API.
 
     Parameters
     ----------
     task : iterable
-        A list containing the indices of samples to be operated on, the filename
-        containing the prior samples, and the data.
+        An array containing the indices of samples to be operated on, the
+        filename containing the prior samples, and the data.
 
     Returns
     -------
     ll : `numpy.ndarray`
         Array of log-likelihood values.
+
     """
     start_stop, filename, data = task
 
@@ -39,30 +40,32 @@ def _marginal_ll_worker(task):
     for i in range(n_chunk):
         try:
             ll[i] = marginal_ln_likelihood(chunk[i], data)
-        except:
+        except Exception as e:
+            log.error(e)
             ll[i] = np.nan
 
     return ll
 
-def get_good_samples(n_samples, filename, data, pool):
+def get_good_sample_indices(n_prior_samples, filename, data, pool):
     """
     Return the indices of 'good' samples by computing the log-likelihood
-    for ``n_samples`` prior samples and doing rejection sampling.
+    for ``n_prior_samples`` prior samples and doing rejection sampling.
 
     For speed when parallelizing, this accepts a filename for an HDF5
     that contains the prior samples, splits up the samples based on the
     number of processes / MPI workers, and only distributes the indices
-    for each worker to read.
+    for each worker to read. This limits the amount of data that needs
+    to be passed around.
 
     Parameters
     ----------
-    n_samples : int
+    n_prior_samples : int
         The number of prior samples to use.
     filename : str
-        Path to an HDF5 file comtaining the prior samples.
+        Path to an HDF5 file containing the prior samples.
     data : `thejoker.data.RVData`
         An instance of ``RVData`` with the data we're modeling.
-    pool : `thejoker.pool.GenericPool` or subclass
+    pool : `~schwimmbad.pool.BasePool` or subclass
         An instance of a processing pool - must have a ``.map()`` method.
 
     Returns
@@ -71,35 +74,39 @@ def get_good_samples(n_samples, filename, data, pool):
         An array of integers for the prior samples that pass
         rejection sampling.
 
+    TODO
+    ----
+    - The structure of this function is ok for most cluster-like machines
+        up to ~2**28 samples or so. Then it becomes an issue that I keep
+        the likelihood values in memory. We may want to be able to cache
+        the likelihood values instead?
+
     """
     if pool.size > 0:
         # try chunking by the pool size
-        chunk_size = n_samples // pool.size
+        chunk_size = n_prior_samples // pool.size
         if chunk_size == 0:
             chunk_size = 1
     else:
         chunk_size = 1
 
     tasks = [[(i*chunk_size, (i+1)*chunk_size), filename, data]
-             for i in range(n_samples//chunk_size+1)]
+             for i in range(n_prior_samples//chunk_size+1)]
 
     results = [r for r in pool.map(_marginal_ll_worker, tasks)]
     marg_ll = np.concatenate(results)
 
-    assert len(marg_ll) == n_samples
+    assert len(marg_ll) == n_prior_samples
 
-    uu = np.random.uniform(size=n_samples)
+    # rejection sample using the marginal likelihood
+    uu = np.random.uniform(size=n_prior_samples)
     good_samples_bool = uu < np.exp(marg_ll - marg_ll.max())
     good_samples_idx, = np.where(good_samples_bool)
-    n_good = good_samples_bool.sum()
-    logger.info("{} good samples after rejection sampling".format(n_good))
 
     return good_samples_idx
 
 def _orbital_params_worker(task):
     """
-
-
     This is meant to be
         ``map``ped using one of the ``Pool`` classes by the functions below and
         is not supposed to be in the public API.
@@ -113,7 +120,7 @@ def _orbital_params_worker(task):
     else:
         seed = idx[0]
     np.random.seed(seed) # TODO: is this good enough?
-    logger.debug("worker with chunk {} has seed {}".format(idx[0], seed))
+    log.debug("worker with chunk {} has seed {}".format(idx[0], seed))
 
     pars = np.zeros((n_chunk, 7))
     with h5py.File(filename, 'r') as f:
@@ -129,7 +136,7 @@ def _orbital_params_worker(task):
             K, *v_terms = np.random.multivariate_normal(p, cov)
 
             if K < 0:
-                # logger.warning("Swapping K")
+                # log.warning("Swapping K")
                 K = np.abs(K)
                 omega += np.pi
                 omega = omega % (2*np.pi) # HACK: I think this is safe
--- thejoker/sampler/sampler.py
+++ thejoker/sampler/sampler.py
@@ -3,18 +3,19 @@ from __future__ import division, print_function
 # Standard library
 import os
 import sys
+import tempfile
 
 # Third-party
-from astropy import log as logger
 import astropy.units as u
 import numpy as np
 
 # Project
+from ..log import log
 from ..data import RVData
 from .params import JokerParams
+from .multiproc_helpers import get_good_sample_indices
 
 class TheJoker(object):
-
     """
     A custom Monte-Carlo sampler for two-body systems.
 
@@ -30,23 +31,8 @@ class TheJoker(object):
         A ``RandomState`` instance to serve as a parent for the random
         number generators. See the :ref:`random-numbers` page for more
         information.
-    **hyper_pars
-        All other kwargs are used to set the hyper-parameter values.
-        These can be:
-
-            - ``P_min`` : `astropy.units.Quantity`
-                Lower bound on prior over period (smallest period considered).
-            - ``P_max`` : `astropy.units.Quantity`
-                Upper bound on prior over period (largest period considered).
-
     """
-    def __init__(self, data, params=None, pool=None, random_state=None, **hyper_pars):
-
-        # validate input data
-        if not isinstance(data, RVData):
-            raise TypeError("Input data must be an RVData instance, not '{}'"
-                            .format(type(data)))
-        self.data = data
+    def __init__(self, params, pool=None, random_state=None):
 
         # set the processing pool
         if pool is None:
@@ -69,20 +55,12 @@ class TheJoker(object):
 
         self.random_state = random_state
 
-        # TODO: validate hyper-parameters
-
         # check if a JokerParams instance was passed in to specify the state
-        if params is None:
-            # accept defaults - global velocity trend with constant offset v0
-            self.params = JokerParams()
+        if not isinstance(params, JokerParams):
+            raise TypeError("Parameter specification must be a JokerParams instance, "
+                            "not a '{}'".format(type(params)))
+        self.params = params
 
-        else:
-            if not isinstance(params, JokerParams):
-                raise TypeError("Parameter specification must be a JokerParams instance, "
-                                "not a '{}'".format(type(params)))
-            self.params = params
-
-    @u.quantity_input(P_min=u.day, P_max=u.day)
     def sample_prior(self, size=1):
         """
         Generate samples from the prior. Logarithmic in period, uniform in
@@ -90,12 +68,8 @@ class TheJoker(object):
 
         Parameters
         ----------
-        n : int
+        size : int
             Number of samples to generate.
-        P_min : `astropy.units.Quantity`
-            Minimum period.
-        P_max : `astropy.units.Quantity`
-            Maximum period.
 
         Returns
         -------
@@ -103,22 +77,117 @@ class TheJoker(object):
             Keys: `['P', 'phi0', 'ecc', 'omega']`, each as
             `astropy.units.Quantity` objects (i.e. with units).
 
+        TODO
+        ----
+        - All prior distributions are essentially fixed. These should be
+            customizable in some way...
+
         """
+        pars = dict()
 
         # sample from priors in nonlinear parameters
-        P = np.exp(np.random.uniform(np.log(P_min.to(u.day).value),
-                                     np.log(P_max.to(u.day).value),
-                                     size=n)) * u.day
-        phi0 = np.random.uniform(0, 2*np.pi, size=n) * u.radian
+        pars['P'] = np.exp(np.random.uniform(np.log(self.params.P_min.to(u.day).value),
+                                             np.log(self.params.P_max.to(u.day).value),
+                                             size=size)) * u.day
+        pars['phi0'] = np.random.uniform(0, 2*np.pi, size=size) * u.radian
 
         # MAGIC NUMBERS below: Kipping et al. 2013 (MNRAS 434 L51)
-        ecc = np.random.beta(a=0.867, b=3.03, size=n)
-        omega = np.random.uniform(0, 2*np.pi, size=n) * u.radian
+        pars['ecc'] = np.random.beta(a=0.867, b=3.03, size=size)
+        pars['omega'] = np.random.uniform(0, 2*np.pi, size=size) * u.radian
 
-        # DFM's idea: wide, Gaussian prior in log(s^2)
-        s2 = np.exp(np.random.normal(log_jitter2_mean, log_jitter2_std, size=n)) * (u.m/u.s)**2
-        # def sample_jitter(mu, sig, size=1):
-        #     U = np.random.uniform(size=size)
-        #     return np.exp(0.5 * (mu - np.sqrt(2)*sig*erfcinv(2*U)))
+        if not self.params._fixed_jitter:
+            # Gaussian prior in log(s^2)
+            log_s2 = np.random.normal(*self.params.jitter, size=size)
+            pars['jitter'] = np.sqrt(np.exp(log_s2)) * self.params._jitter_unit
 
-        return dict(P=P, phi0=phi0, ecc=ecc, omega=omega, jitter2=s2)
+        return pars
+
+    def _rejection_sample_from_cache(self, data, n_prior_samples, cache_file):
+        """
+        """
+
+        # Get indices of good samples from the cache file
+        # TODO: I have some implementation questions about whether this should return
+        #   a boolean array (in which case I need to process all likelihood values) or
+        #   an array of integers...Right now, _marginal_ll_worker has to return the values
+        #   because we then compare with the maximum value of the likelihood
+        good_samples_idx = get_good_sample_indices(n_prior_samples, cache_file, data, self.pool)
+        if len(good_samples_idx) == 0:
+            log.error("Failed to find any good samples!")
+            self.pool.close()
+            sys.exit(0)
+
+        # log.info("{} good samples after rejection sampling".format(n_good))
+
+        # compute orbital parameters for all good samples
+        # orbital_params = samples_to_orbital_params(good_samples_idx, tmp_prior_filename,
+        #                                            data, pool, seed)
+
+        pass
+
+    def rejection_sample(self, data, n_prior_samples=None, prior_cache_file=None):
+        """
+        Parameters
+        ----------
+        data :
+        n_prior_samples :
+        prior_cache_file :
+        """
+
+        # validate input data
+        if not isinstance(data, RVData):
+            raise TypeError("Input data must be an RVData instance, not '{}'"
+                            .format(type(data)))
+        self.data = data
+
+        if n_prior_samples is None and prior_cache_file is None:
+            raise ValueError("You either have to specify the number of prior samples "
+                             "to generate, or a path to a file containing cached prior "
+                             "samples in (TODO: what format?). If you want to try an "
+                             "experimental adaptive method, try .rejection_sample_adapt()")
+
+        if prior_cache_file is not None:
+            return self._rejection_sample_from_cache(data, n_prior_samples, prior_cache_file)
+
+        else:
+            with tempfile.NamedTemporaryFile(mode='r+') as f:
+                # first do prior sampling, cache to file
+                prior_samples = self.sample_prior(size=n_prior_samples)
+
+                # TODO: save samples to file
+
+                return self._rejection_sample_from_cache(data, n_prior_samples, f.name)
+
+    # def rejection_sample_adapt(self, data, min_n, prior_chunk_size=1024, max_prior_samples=2**24,
+    #                            prior_cache_file=None):
+    #     """
+    #     """
+
+    #     # TODO: always cache prior samples for simplicity?!
+
+    #     if prior_cache_file is None and isinstance(self.pool, schwimmbad.SerialPool):
+    #         maxiter = 1000 # TODO: make arg?
+    #         iters = 0
+    #         n_good_samples = 0
+    #         n_prior_samples = prior_chunk_size
+    #         while n_good_samples < n and n_prior_samples < max_prior_samples and iters < maxiter:
+    #             prior_samples = self.sample_prior(size=n_prior_samples)
+    #             good_samples = self._rejection_sample(prior_samples, data) # what else?
+
+    #             if len(good_samples) > 1:
+    #                 # TODO: save the samples
+    #                 pass
+
+    #             else:
+    #                 # ignore the sample because only one was returned
+    #                 pass
+
+    #             n_prior_samples *= 2
+
+    #         with tempfile.NamedTemporaryFile(mode='r+') as f:
+    #             # TODO: generate prior samples, save to
+    #             pass
+
+    #     else:
+    #         # need to read samples from filename
+    #         pass
