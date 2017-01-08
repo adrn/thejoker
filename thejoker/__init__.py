4aa0db4e028ad5158d3074d9c94d902473b27024
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-01-08 12:45:36 -0500

    remove paths

--- thejoker/__init__.py
+++ thejoker/__init__.py
@@ -2,6 +2,5 @@ from ._astropy_init import *
 
 # For egg_info test builds to pass, put package imports here.
 if not _ASTROPY_SETUP_:
-    from .paths import *
     from .celestialmechanics import *
     from .sampler import *
