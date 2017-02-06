11efc818e3df3332d0a9cd6f4b42b1cf49e2d5c9
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-02-05 22:28:12 -0500

    use astropy docs style

--- docs/conf.py
+++ docs/conf.py
@@ -104,8 +104,8 @@ version = package.__version__.split('-', 1)[0]
 release = package.__version__
 
 # Use astropy plot style
-from astropy.visualization import mpl_style
-plot_rcparams = mpl_style
+from astropy.visualization.mpl_style import astropy_mpl_docs_style
+plot_rcparams = astropy_mpl_docs_style
 if not ON_TRAVIS:
     plot_rcparams['text.usetex'] = True
 plot_rcparams['savefig.facecolor'] = 'none'
