eb3f8ea9db67a1db6087c56b1087408e2bd530ea
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-08-15 13:56:10 +0200

    only require import if we're building

--- thejoker/sampler/setup_package.py
+++ thejoker/sampler/setup_package.py
@@ -6,18 +6,21 @@ def get_extensions():
     exts = []
 
     from os import path
-    import twobody
-    twobody_path = path.dirname(twobody.__file__)
+    try:
+        # Get twobody path, or ignore failure -- for egg_info build
+        import twobody
+        twobody_path = path.dirname(twobody.__file__)
+    except ImportError:
+        twobody_path = None
 
     cfg = setup_helpers.DistutilsExtensionArgs()
     cfg['include_dirs'].append('numpy')
-    cfg['include_dirs'].append(twobody_path)
+
+    if twobody_path is not None:
+        cfg['include_dirs'].append(twobody_path)
+
     cfg['extra_compile_args'].append('--std=gnu99')
     cfg['sources'].append('thejoker/sampler/fast_likelihood.pyx')
-    # cfg['sources'].append('twobody/celestial/src/twobody.c')
     exts.append(Extension('thejoker.sampler.fast_likelihood', **cfg))
 
     return exts
-
-# def get_package_data():
-#     return {'twobody.celestial': ['src/*.h', 'src/*.c', '*.pyx', '*.pxd']}
