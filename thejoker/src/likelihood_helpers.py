a2afbe65bc5d73de78f1c4220e653341e7f01802
Adrian Price-Whelan <adrian.prw@gmail.com>
2019-11-04 10:23:07 -0500

    get M

---
+++ thejoker/src/likelihood_helpers.py
@@ -0,0 +1,18 @@
+
+def get_linear_design_matrix(thejoker, data, ids=None):
+    """Construct the portion of the design matrix relevant for the linear
+    parameters of The Joker beyond the amplitude, ``K``.
+    """
+
+    if ids is None:
+        ids = np.zeros(len(data), dtype=int)
+    ids = np.array(ids)
+
+    unq_ids = np.unique(ids)
+    constant_part = np.zeros((len(data), len(unq_ids)))
+
+    constant_part[:, 0] = 1.
+    for id_ in unq_ids:
+        constant_part[ids == id_, 1] = 1.
+
+    return constant_part
