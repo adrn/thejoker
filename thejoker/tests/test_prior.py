84e4b4d0b91c79df9fc3cc93a0da5e4669c1bc70
Adrian Price-Whelan <adrian.prw@gmail.com>
2019-11-04 12:06:37 -0500

    rewrite tests

--- thejoker/tests/test_prior.py
+++ thejoker/tests/test_prior.py
@@ -8,7 +8,7 @@ import pytest
 import theano.tensor as tt
 
 # Project
-from ..prior import JokerPrior
+from ..prior import JokerPrior, default_nonlinear_prior, default_linear_prior
 
 
 def get_valid_objs():
@@ -22,92 +22,74 @@ def get_valid_objs():
     # default units expected for output
     default_expected_units = {'P': u.day, 'e': u.one,
                               'omega': u.radian, 'M0': u.radian,
-                              'jitter': u.m/u.s}
+                              'jitter': u.m/u.s,
+                              'K': u.km/u.s, 'v0': u.km/u.s}
 
-    # Test valid initialization patterns:
-    priors.append(dict(P_lim=[1, 100] * u.day))  # legacy
+    # Default prior with standard parameters:
+    priors.append(dict(P_min=1*u.day, P_max=100*u.day,
+                       sigma_K0=25*u.km/u.s, sigma_v0=100*u.km/u.s))
     expected_units.append(default_expected_units)
+
+    # Get default pars
+    with pm.Model() as model:
+        nl_pars, nl_unpars = default_nonlinear_prior(P_min=1*u.day,
+                                                     P_max=1e5*u.day)
+        l_pars, l_unpars = default_linear_prior(nl_pars,
+                                                sigma_K0=25*u.km/u.s,
+                                                sigma_v0=100*u.km/u.s)
+    default_pars = {**nl_pars, **l_pars}
+    default_unpars = {**nl_unpars, **l_unpars}
 
     # No transformed parameters
+    pars = {}  # pars to replace
+    unpars = {}
+    units = default_expected_units.copy()
     with pm.Model() as model:
-        P = xu.with_unit(pm.Normal('P', 10, 0.5),
-                         u.day)
-        e = xu.with_unit(pm.Uniform('e', 0, 1),
-                         u.one)
-        omega = xu.with_unit(pm.Normal('omega', 0.5, 1),
-                             u.radian)
+        pars['P'] = xu.with_unit(pm.Normal('P', 10, 0.5),
+                                 u.year)
+        units['P'] = u.year
 
-    priors.append(dict(pars=P))
-    expected_units.append(default_expected_units)
+        pars['e'] = xu.with_unit(pm.Uniform('e', 0, 1),
+                                 u.one)
 
-    priors.append(dict(pars={'P': P}))
-    expected_units.append(default_expected_units)
+        pars['omega'] = xu.with_unit(pm.Normal('omega', 25., 1),
+                                     u.degree)
+        units['omega'] = u.degree
 
-    priors.append(dict(pars=[P, e]))
-    expected_units.append(default_expected_units)
+    for k in default_pars.keys():
+        if k not in pars:
+            pars[k] = default_pars[k]
 
-    priors.append(dict(pars=(P, e)))
-    expected_units.append(default_expected_units)
+            if k in default_unpars:
+                unpars[k] = default_unpars[k]
 
-    priors.append(dict(pars=[P, omega]))
-    expected_units.append(default_expected_units)
+    priors.append(dict(pars=pars))
+    expected_units.append(units)
 
-    priors.append(dict(pars=[P, omega, e]))
-    expected_units.append(default_expected_units)
+    priors.append(dict(pars=pars, unpars=unpars))
+    expected_units.append(units)
 
-    priors.append(dict(pars={'P': P, 'e': e, 'omega': omega}))
-    expected_units.append(default_expected_units)
-
+    # Additional transformed parameter
+    pars = {}  # pars to replace
+    unpars = {}
+    units = default_expected_units.copy()
     with pm.Model() as model:
-        # Unit-less eccentricity
-        e_nounit = pm.Uniform('e', 0, 1)
-
-        # Omega in different units:
-        omega_deg = xu.with_unit(pm.Uniform('omega', 5, 20),
-                                 u.degree)
-
-    priors.append(dict(pars=[P, e_nounit]))
-    expected_units.append(default_expected_units)
-
-    priors.append(dict(pars=[P, omega_deg]))
-    _units = default_expected_units.copy()
-    _units['omega'] = u.deg
-    expected_units.append(_units)
-
-    # Transformed parameter
-    with pm.Model() as model:
-        logP = pm.Uniform('logP',
-                          lower=np.log10(1),
-                          upper=np.log10(1e5))
-        P_tr = xu.with_unit(pm.Deterministic('P', 10**logP),
-                            u.year)
-
         logs = pm.Normal('logs', -1, 0.5)
         jitter = xu.with_unit(pm.Deterministic('jitter', tt.exp(logs)),
                               u.km/u.s)
+        unpars['jitter'] = logs
+        pars['jitter'] = jitter
+    units['jitter'] = u.km/u.s
 
-    _units = default_expected_units.copy()
-    _units['P'] = u.year
+    for k in default_pars.keys():
+        if k not in pars:
+            pars[k] = default_pars[k]
 
-    priors.append(dict(pars=P_tr, unpars={'P': logP}))
-    expected_units.append(_units)
+            if k in default_unpars:
+                unpars[k] = default_unpars[k]
 
-    priors.append(dict(pars=[P_tr], unpars={'P': logP}))
-    expected_units.append(_units)
-
-    priors.append(dict(pars=[P_tr, e, omega],
-                       unpars={'P': logP}))
-    expected_units.append(_units)
-
-    priors.append(dict(pars={'P': P_tr, 'e': e},
-                       unpars={'P': logP}))
-    expected_units.append(_units)
-
-    priors.append(dict(pars=[P_tr, e, omega, jitter],
-                       unpars={'P': logP, 'jitter': logs}))
-    _units = _units.copy()
-    _units['jitter'] = u.km/u.s
-    expected_units.append(_units)
+    priors.append(dict(pars=pars, unpars=unpars))
+    expected_units.append(units)
 
     return priors, expected_units
 
@@ -115,12 +97,12 @@ def get_valid_objs():
 @pytest.mark.parametrize('kw,expected_units', list(zip(*get_valid_objs())))
 def test_init_sample(kw, expected_units):
     # Running this function is enough to test valid initialization schemes:
-    if 'P_lim' in kw:
-        prior = JokerPrior.uniform_logP(*kw['P_lim'])
+    if 'P_min' in kw:
+        prior = JokerPrior.from_default(**kw)
     else:
         prior = JokerPrior(**kw)
 
     samples = prior.sample()
-    for k in samples.keys():
+    for k in samples.param_names:
         assert hasattr(samples[k], 'unit')
         assert samples[k].unit == expected_units[k]
