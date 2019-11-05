6dfdecfa7bdd602dd3883c6f4240ef285de5a7a3
Adrian Price-Whelan <adrian.prw@gmail.com>
2019-11-05 12:09:04 -0500

    test combining data objects

--- thejoker/data_helpers.py
+++ thejoker/data_helpers.py
@@ -50,3 +50,56 @@ def guess_time_format(time_val):
 
     else:
         raise ValueError(err_msg)
+
+
+def _prepare_multi_data(data):
+    """Internal function.
+
+    Used to take an input ``RVData`` instance, or a list/dict of ``RVData``
+    instances, and produce concatenated time, RV, and error arrays, along
+    with a consistent t0.
+    """
+    from .data import RVData
+    if isinstance(data, RVData):  # single instance
+        return data, None
+
+    # Turn a list-like into a dict object:
+    if not hasattr(data, 'keys'):
+        _d = {}
+        for i, d in enumerate(data):
+            _d[i] = d
+        data = _d
+
+    # If we've gotten here, data is dict-like:
+    rv_unit = None
+    t = []
+    rv = []
+    err = []
+    ids = []
+    for k in data.keys():
+        d = data[k]
+
+        if d._has_cov:
+            raise NotImplementedError("We currently don't support "
+                                      "multi-survey data when a full "
+                                      "covariance matrix is specified. "
+                                      "Raise an issue in adrn/thejoker if "
+                                      "you want this functionality.")
+
+        if rv_unit is None:
+            rv_unit = d.rv.unit
+
+        t.append(d.t.tcb.mjd)
+        rv.append(d.rv.to_value(rv_unit))
+        err.append(d.rv_err.to_value(rv_unit))
+        ids.append([k] * len(d))
+
+    t = np.concatenate(t)
+    rv = np.concatenate(rv) * rv_unit
+    err = np.concatenate(err) * rv_unit
+    ids = np.concatenate(ids)
+
+    all_data = RVData(t=Time(t, format='mjd', scale='tcb'),
+                        rv=rv, rv_err=err)
+
+    return all_data, ids
